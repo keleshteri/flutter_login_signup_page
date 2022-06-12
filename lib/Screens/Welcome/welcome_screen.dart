5bfdafa7e37fe352a77a6ed747d87a06a02533d8
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2022-06-13 00:42:29 +0800

    Part 1

---
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_signup_page/Screens/Welcome/components/body.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  // const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+      body: Body(),
+    );
+  }
+}
